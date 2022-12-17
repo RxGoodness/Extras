ce5a3723fd9519c1242074d44beb6740091578cc
RxGoodness <adedokuntoyyib@gmail.com>
2022-12-17 13:39:40 +0100

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -119,17 +119,21 @@ export class AuthService {
     return this.signToken(user.id, user.email)
     // return "I log in here";
   }
-  signToken(
+  async signToken(
     userId: number,
     email: string,
-  ): Promise<string> {
+  ): Promise<{access_token: string}> {
     const payload = {
         sub: userId, email
     }
-    const secret = this.config.get('JWT_SECRET')
-    return this.jwt.signAsync(payload, {
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
         expiresIn: '15m',
         secret: secret
     })
+
+    return {
+        access_token: token
+    }
   }
 }
