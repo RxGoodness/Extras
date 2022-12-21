f94c42921f29adbea709f9a7c0eba208a3262dce
RxGoodness <adedokuntoyyib@gmail.com>
2022-12-21 17:42:46 +0100

    bookmark

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
 
-@Controller('bookmark')
-export class BookmarkController {}
+@UseGuards(JwtGuard)
+@Controller('bookmarks')
+export class BookmarkController {
+    @Get()
+    getBookmarks(){}
+
+    @Post()
+    createBookmark(){}
+
+    @Get()
+    getBookmarkById(){}
+
+    @Patch()
+    editBookmarkById(){}
+
+    @Delete()
+    deleteBookmarkById(){}
+
+}
