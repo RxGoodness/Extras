67510eb0ab9417c1bec79a9fd46af198e8758946
RxGoodness <adedokuntoyyib@gmail.com>
2022-12-21 18:24:50 +0100

    bookmark

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -3,17 +3,28 @@ import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
 @Injectable()
 export class BookmarkService {
-
+ constructor(private bookmarkService:BookmarkService){}
     getBookmarks(userId: number){
         return this.bookmarkService.getBookmarks(userId)
     }
 
-    createBookmark(userId: number, dto: CreateBookmarkDto){}
+    createBookmark(userId: number, dto: CreateBookmarkDto){
+        return this.bookmarkService.createBookmark(userId, dto)
 
-    getBookmarkById(userId: number, bookmarkId: number){}
+    }
 
-    editBookmarkById(userId: number, dto: EditBookmarkDto){}
+    getBookmarkById(userId: number, bookmarkId: number){
+        return this.bookmarkService.getBookmarkById(userId, bookmarkId)
+        
+    }
 
-    deleteBookmarkById(userId: number, bookmarkId: number){}
+    editBookmarkById(userId: number, bookmarkId: number, dto: EditBookmarkDto){
+        return this.bookmarkService.editBookmarkById(userId, bookmarkId, dto)
+
+    }
+
+    deleteBookmarkById(userId: number, bookmarkId: number){
+        return this.bookmarkService.deleteBookmarkById(userId, bookmarkId)
+    }
 
 }
