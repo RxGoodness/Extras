dc9419fbccab194d2538edaf87f6a524be9a1b01
RxGoodness <adedokuntoyyib@gmail.com>
2022-12-21 18:13:43 +0100

    bookmark

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+
+    getBookmarks(userId: number){
+        return this.bookmarkService.getBookmarks(userId)
+    }
+
+    createBookmark(userId: number, dto: CreateBookmarkDto){}
+
+    getBookmarkById(userId: number, bookmarkId: number){}
+
+    editBookmarkById(userId: number, dto: EditBookmarkDto){}
+
+    deleteBookmarkById(userId: number, bookmarkId: number){}
+
+}
